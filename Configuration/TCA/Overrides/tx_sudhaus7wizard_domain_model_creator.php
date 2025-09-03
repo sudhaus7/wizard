52826b4004af1d469841f54ac83fe3c68bd67623
Frank Berger <fberger@sudhaus7.de>
2025-09-03 16:26:21 +0200

    fix errormessage about ext_tables.php in 'check for broken extensions' module

--- Configuration/TCA/Overrides/tx_sudhaus7wizard_domain_model_creator.php
+++ Configuration/TCA/Overrides/tx_sudhaus7wizard_domain_model_creator.php
@@ -11,4 +11,13 @@
  * The TYPO3 project - inspiring people to share!
  */
 
+use SUDHAUS7\Sudhaus7Wizard\Backend\TCA\UpdateStatus;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = UpdateStatus::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][]  = UpdateStatus::class;
+
+ExtensionManagementUtility::addToInsertRecords('tx_sudhaus7wizard_domain_model_creator');
+
+
 $GLOBALS['TCA']['tx_sudhaus7wizard_domain_model_creator']['ctrl']['security']['ignorePageTypeRestriction'] = true;
